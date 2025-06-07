9916a06916e83c058dab4cb1c2b2cf4039da0abf
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-06-07 18:07:05 +0500

    Add Xata integration with database configuration and migration setup

---
+++ xata.ts
@@ -0,0 +1,27 @@
+import { buildClient } from '@xata.io/client'
+import type { BaseClientOptions } from '@xata.io/client'
+
+export type DatabaseSchema = {}
+
+const DatabaseClient = buildClient()
+
+const defaultOptions = {
+        databaseURL: 'https://Zoirjon-Zokirjonov-s-workspace-m901tb.eu-central-1.xata.sh/db/loom_db',
+        apiKey: process.env.XATA_API_KEY,
+        branch: 'main',
+}
+
+export class XataClient extends DatabaseClient<DatabaseSchema> {
+        constructor(options?: BaseClientOptions) {
+                super({ ...defaultOptions, ...options })
+        }
+}
+
+let instance: XataClient | undefined = undefined
+
+export const getXataClient = () => {
+        if (instance) return instance
+
+        instance = new XataClient()
+        return instance
+}
