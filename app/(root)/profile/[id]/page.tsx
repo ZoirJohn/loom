ab20b9f85e121a94f8b6af7a9d214f8d73530fa9
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-06-14 17:22:28 +0500

    Add vidoes to main page of the application

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,24 +1,42 @@
+import EmptyState from '@/components/EmptyState'
 import Header from '@/components/Header'
 import VideoCard from '@/components/VideoCard'
-import { dummyCards } from '@/constants'
+import { getAllVideosByUser } from '@/lib/actions/videos'
+import { redirect } from 'next/navigation'
 
-export default async function Profile({ params }: ParamsWithSearch) {
+export default async function Profile({ params, searchParams }: ParamsWithSearch) {
         const { id } = await params
+        const { query, filter } = await searchParams
+        const { user, videos } = await getAllVideosByUser(id, query, filter)
+        if (!user) redirect('/404')
         return (
                 <main className='wrapper page'>
                         <Header
-                                subHeader='suppport@yopmail.com'
-                                title='Yopmail'
-                                userImg='/assets/images/dummy.jpg'
+                                subHeader={user.email}
+                                title={user.name}
+                                userImg={user.image || ''}
                         />
-                        <section className='video-grid'>
-                                {dummyCards.map((card) => (
-                                        <VideoCard
-                                                {...card}
-                                                key={card.id}
-                                        />
-                                ))}
-                        </section>
+                        {videos.length > 0 ? (
+                                <section className='video-grid'>
+                                        {videos.map(({ video, user }) => {
+                                                return (
+                                                        <VideoCard
+                                                                {...video}
+                                                                key={video.id}
+                                                                thumbnail={video.thumbnailUrl}
+                                                                userImg={user?.image || ''}
+                                                                username={user?.name || 'Guest'}
+                                                        />
+                                                )
+                                        })}
+                                </section>
+                        ) : (
+                                <EmptyState
+                                        icon='/assets/icons/video.svg'
+                                        title='No videos found'
+                                        description='Videos will show up once you upload them'
+                                />
+                        )}
                 </main>
         )
 }
